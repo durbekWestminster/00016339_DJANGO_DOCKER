0c0ead2dfb06bd72e90bc7953e93afd4c8ddd27c
durbek <d.ataboyev@hometasker.io>
2026-02-26 21:49:35 +0500

    add health check

--- eventplanner/urls.py
+++ eventplanner/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("events.urls")),
+    path("health/", health_check),
+
 ]
